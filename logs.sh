6c5be6b3e2270feece167a945ac7226dfe433b45
prasanth <durgaprasanth003@gmail.com>
2026-05-30 16:22:22 +0530

    hello world

---
+++ logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+PATH="/var/logs/shell-logs"
+SCRIPT_NAME="$0"
+LOG_FILE="$PATH/$SCRIPT_NAME.log" 
+
+set -e 
+
+check () {
+    echo error at $1 : $2
+}
+
+trap 'check "${LINE_NO}" : "${BASH_COMMAND}" ' ERR
+
+
+mkdir -p /var/logs/shell-logs
+
+if [ $USER -eq 0 ]
+then 
+    echo " user has root access "
+else 
+    echo " user don't have root access "
+    exit 1 
+fi 
+
+dnf install ngijne3jd20 &>> $LOG_FILE
+
+
+
+
